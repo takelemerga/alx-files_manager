d68814a05dccebfce6d12774e09818c52e1a6cd6
tolaa <mgamtaa@gmail.com>
2023-03-08 22:32:08 +0300

    server

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = require('./routes/index');
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
