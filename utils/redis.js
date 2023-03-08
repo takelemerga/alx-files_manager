4a529e664322f737b0fc4b00f7908b85048c0477
tolaa <mgamtaa@gmail.com>
2023-03-08 09:10:43 +0300

    create redis client

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+
+const rdsClient = class RedisClient{
+  constructor(){
+    this.client = redis.createClient();
+    this.connected = true;
+    this.client.on('connect', () => {
+      this.connected = true; 
+    });
+    this.client.on('error', (err) => {
+      this.connected = false;
+      console.log(`Redis client not connected to the server: ${err.message}`);
+    });
+  }
+
+  isAlive(){
+    return this.connected;
+  }
+
+  async get(key) {
+    const getValue = await promisify(this.client.get).bind(this.client);
+    return await getValue(key); 
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  // delete function
+  async del(key) {
+    this.client.del(key);
+  }
+};
+const redisClient = new rdsClient();
+module.exports = redisClient; 
