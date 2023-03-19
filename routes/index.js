4c1cbdcaae5e02c7e85616ead217b1d9f7bc2c43
tolaa <mgamtaa@gmail.com>
2023-03-19 10:13:17 +0300

    all end point index

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,14 @@ import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
-//router.get('/status', AppController.getStatus);
-//router.get('/stats', AppController.getStats);
-// should return if Redis is alive and if the DB is alive
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+// return if Redis is alive and if the DB is alive too
+router.get('/status', (req, res) => {
+  AppController.getStatus(req, res);
+});
 
-  // should return the number of users and files in DB
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+// return the number of users and files in DB
+router.get('/stats', (req, res) => {
+  AppController.getStats(req, res);
+});
 
 module.exports = router;
