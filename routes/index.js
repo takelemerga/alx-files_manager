d0194542ab5fc1451f7acaf808e8bed3e6579001
tolaa <mgamtaa@gmail.com>
2023-03-20 20:39:38 +0300

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
@@ -20,5 +21,9 @@ router.get('/stats', (req, res) => {
 }); */
 
 router.post('/users', UsersController.postNew);
+
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
