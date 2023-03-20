68146d793bd12d7b13e7b177603db46de9125bf1
tolaa <mgamtaa@gmail.com>
2023-03-20 11:18:23 +0300

    create user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,17 +6,17 @@ class UsersController {
     const email = req.body ? req.body.email : null;
     const password = req.body ? req.body.password : null;
 
-    const emailExist = await (await dbClient.usersCollection()).findOne({email}); 
+    const emailExist = await (await dbClient.usersCollection()).findOne({ email });
 
     if (!email) return (res.status(400).json({ error: 'Missing email' }));
     if (!password) return (res.status(400).json({ error: 'Missing password' }));
     if (emailExist) return res.status(400).json({ error: 'Already exist' });
 
     const hashpwd = sha1(password);
-   
+
     const addUser = await (await dbClient.usersCollection('users')).insertOne({ email, password: hashpwd });
     const newUser = { id: addUser.ops[0]._id, email: addUser.ops[0].email };
-    return res.status(201).json(newUser); 
+    return res.status(201).json(newUser);
   }
 }
 
