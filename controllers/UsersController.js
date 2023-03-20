4aa6cb2e9ee15f166a47dae9a0abc403cebf98be
tolaa <mgamtaa@gmail.com>
2023-03-20 20:40:25 +0300

    updated

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,8 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import RedisClient from '../utils/redis';
+
+const { ObjectId } = require('mongodb');
 
 class UsersController {
   static async postNew(req, res) {
@@ -17,6 +20,20 @@ class UsersController {
     const addUser = await (await dbClient.usersCollection('users')).insertOne({ email, password: hashpwd });
     const newUser = { id: addUser.ops[0]._id, email: addUser.ops[0].email };
     return res.status(201).json(newUser);
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token') || null;
+    if (!token) return response.status(401).send({ error: 'Unauthorized' });
+
+    const redisToken = await RedisClient.get(`auth_${token}`);
+    if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
+
+    const user = await (await dbClient.usersCollection('users')).findOne({ _id: ObjectId(redisToken) });
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    delete user.password;
+
+    return response.status(200).send({ id: user._id, email: user.email });
   }
 }
 
